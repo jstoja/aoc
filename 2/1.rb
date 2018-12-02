13636d169180efd1d6f5899b2098d03d954b628b
Julien BORDELLIER <me@julienbordellier.com>
2018-12-02 13:06:35 +0100

    day2

---
+++ 2/1.rb
@@ -0,0 +1,7 @@
+data = []
+
+File.foreach('input1.txt') do |line|
+  g = line.each_char.group_by { |c| line.count c }
+  data << g.keys.select { |n| (n.eql? 2) || (n.eql? 3) }
+end
+puts data.flatten.group_by { |x| x }.values.map(&:count).reduce(1, :*)
---
+++ 2/2.rb
@@ -0,0 +1,31 @@
+data = []
+
+def diff_1_letter(a, b)
+  diff_position = []
+  a.length.times do |i|
+    if !a[i].eql? b[i]
+      diff_position << i
+    elsif diff_position.length > 1
+      return 0
+    end
+  end
+  if diff_position.length == 1
+    diff_position.first
+  else
+    0
+  end
+end
+
+File.foreach('input2.txt') do |line|
+  n = line.chomp
+  data << n
+  data.each do |s|
+    d = diff_1_letter(n, s)
+    unless d.zero?
+      str = n.chars
+      str.delete_at(d)
+      puts str.join
+      exit
+    end
+  end
+end
